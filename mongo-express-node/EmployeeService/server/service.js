3101ea5d8ace6b87c9d5da5c679f62dee22f4c4b
Chinmay Vivek <chinmay.vivek@gmail.com>
2020-04-01 21:20:23 +0530

    [REM]: blank space removed

--- mongo-express-node/EmployeeService/server/service.js
+++ mongo-express-node/EmployeeService/server/service.js
@@ -1,13 +1,5 @@
 const { app } = require("../endpoint/app");
 
-
-
-// var = require('express');
-// var cor = require('cors');
-// var = require ('mongoose');
-// var = require ('body-parser');
-// var jwt = require('jsonwebtoken');
-
 // Hosting
 app.listen(4000,function(){
     console.log('service started');
