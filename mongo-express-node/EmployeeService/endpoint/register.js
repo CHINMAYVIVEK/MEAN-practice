88b9455acef4de4961bdd72cf18cb107b20e04df
Chinmay Vivek <chinmay.vivek@gmail.com>
2020-04-02 00:40:04 +0530

    [UPDT]: login API updated

--- mongo-express-node/EmployeeService/endpoint/register.js
+++ mongo-express-node/EmployeeService/endpoint/register.js
@@ -1,13 +1,16 @@
 const { mems } = require("../models/model_dhar");
+
 var exp = require('express');
 var bp = require('body-parser');
 // Create Restful API for Routing (httpGet, httpPost, HttpDelete)
+
 var app = exp(); // express give features for routing and hosting
 
 exports.app = app;
 app.use(bp.urlencoded({ extended: false }));
 app.use(bp.json());
-// http://localhost:port/Register
+
+// http://localhost:4000/Register
 app.post('/Register', function (req, res) {
     let userData = req.body;
     var ptr = new mems(userData);
@@ -17,6 +20,29 @@ app.post('/Register', function (req, res) {
         }
         else {
             res.send('saved');
+        }
+    });
+});
+
+// http://localhost:4000/Login
+app.post('/Login', function (req, res) {
+    let userData = req.body;
+    mems.findOne({ username: userData.username }, function (error, user) {
+        if (error) {
+            console.log('error');
+        }
+        else {
+            if (!user) {
+                res.status(401).send('invalid username');
+            }
+            else {
+                if (user.password !== userData.password) {
+                    res.status(401).send('invalid password');
+                }
+                else {
+                    res.send(user);
+                }
+            }
         }
     });
 });
--- mongo-express-node/EmployeeService/models/model_dhar.js
+++ mongo-express-node/EmployeeService/models/model_dhar.js
@@ -1,10 +1,11 @@
 const { mem, url } = require("../configuration/db");
+
 var mon = require('mongoose');
 // created structure mem
 var mems = mon.model('dhar', mem, 'users'); // (model , structure, collection)
 
 exports.mems = mems;
-mon.connect(url, { useNewUrlParser: true }, function (err) {
+mon.connect(url, {useNewUrlParser: true, useUnifiedTopology: true}, function (err) {
     if (err) {
         console.log('not connected');
     }
